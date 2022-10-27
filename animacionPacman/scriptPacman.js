b9693e29c5e46ac0f6f59a095403082f68d60530
Emmanuel Robles <rore021226@gs.utm.mx>
2022-10-26 23:28:22 -0500

    Detecta colision con x pero no con y

--- animacionPacman/scriptPacman.js
+++ animacionPacman/scriptPacman.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext('2d');
 let img = new Image();
 img.src = "resource.png";
 let x = canvas.width;
-let key = 39;
+let key = 0;
 let xActual = 0;
 let yActual = 0;
 
@@ -14,9 +14,16 @@ const cantInc = 20;
 const y = canvas.height;
 let direccion = 0;
 let xBarrera = 150;
-let yBarrera = 0;
+let yBarrera = 50;
 let anchuraBarrera = 30;
 let alturaBarrera = 10;
+let barreraBool = false;
+let resourceX = 492;
+const resourceY = 60;
+const pacmanTam = 13;
+const pacmanCantiMov = 3.75;
+const barrera = canvas.width;
+
 
 document.addEventListener('keydown', manejarTecladoAbajo, false);
 //document.addEventListener('keyup',manejarTecladoArriba, false);
@@ -47,21 +54,17 @@ function manejarTecladoAbajo(e) {
 //Vamos a definir los valores de las direcciones para cambiar la imagen del pacman
 //2 arriba, 1 izquierda, 0 abajo, 3 derecha
 
-
 function dibujar(direccion) {
-    let resourceX = 492;
-    const resourceY = 60;
-    const pacmanTam = 13;
-    const pacmanCantiMov = 3.75;
-    const barrera = canvas.width;
 
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     dibujaBarrera();
-    //console.log("xactual: ",xActual,yActual);
+    detectarBarrera();
+    console.log(xActual, yActual)
+
     if (key === 39) {
         ctx.drawImage(img, resourceX + inc, resourceY, pacmanTam, pacmanTam, xActual, yActual, pacmanTam, pacmanTam);
-        if ((xActual < x - (pacmanTam + pacmanCantiMov))){
+        if ((xActual < x - (pacmanTam + pacmanCantiMov)) && !barreraBool) {
             xActual += pacmanCantiMov;
             inc += cantInc - 1;
         } else inc = cantInc;
@@ -70,7 +73,7 @@ function dibujar(direccion) {
 
     if (key === 37) {
         ctx.drawImage(img, resourceX + inc, resourceY + 58, pacmanTam, pacmanTam, xActual, yActual, pacmanTam, pacmanTam);
-        if ((xActual > 0)) {
+        if ((xActual > 0) && !barreraBool) {
             xActual -= pacmanCantiMov;
             inc += cantInc;
         } else inc = cantInc;
@@ -95,31 +98,25 @@ function dibujar(direccion) {
         if (inc > 60) inc = 0;
     }
 }
+
 //En esta seccion del codigo encontraremos los casos para los escenarios segun el nivel, como ahora no existe tal situacion
 //se encontraran datos sobre las pruebas para la deteccion de barreras y otros casos a determinar
-function dibujaEscenario(){
+function dibujaEscenario() {
     dibujaBarrera();
 }
 
-
-/*
-    let ladrillos = [];
-    for (c=0;c<ladrillosColumnas;c++){
-    ladrillos[c]=[];//cada columna ahora sera un arreglo
-    for (r=0;r<ladrillosFilas;r++){
-        ladrillos[c][r]={
-            x:0,
-            y:0,
-            orientacion:
-        }
-    }
-}*/
-
-function dibujaBarrera(){
+function dibujaBarrera() {
     console.log("dibuja barrera");
     ctx.beginPath();
-    ctx.rect(150,0,30,10);
+    ctx.rect(xBarrera, yBarrera, anchuraBarrera, alturaBarrera);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
+}
+
+function detectarBarrera() {
+    if ((xActual >= xBarrera - pacmanTam - 2) && yActual <= yBarrera + alturaBarrera && (xActual <= xBarrera + anchuraBarrera) && yActual >= yBarrera - pacmanTam) {
+        return barreraBool = true;
+    }
+    barreraBool = false;
 }
